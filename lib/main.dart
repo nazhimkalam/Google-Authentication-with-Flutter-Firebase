bd2fafd31c01c853f71c5d2442e5fe878be03114
Nazhim <nazhimkalam@gmail.com>
2021-02-10 00:33:45 +0530

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,77 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 
-void main() {
-  runApp(MyApp());
+void main() => runApp(App());
+
+class App extends StatefulWidget {
+  @override
+  _AppState createState() => _AppState();
 }
 
-class MyApp extends StatelessWidget {
+class _AppState extends State<App> {
+  FirebaseAuth _auth = FirebaseAuth.instance;
+  FirebaseUser _user;
+  GoogleSignIn _googleSignIn = new GoogleSignIn();
+
+  bool isSignIn = false;
+
+  Future<void> handleSignIn() async {
+    GoogleSignInAccount googleSignInAccount = await _googleSignIn.signIn();
+    GoogleSignInAuthentication googleSignInAuthentication =
+        await googleSignInAccount.authentication;
+    AuthCredential credential = GoogleAuthProvider.getCredential(
+        idToken: googleSignInAuthentication.idToken,
+        accessToken: googleSignInAuthentication.accessToken);
+    AuthResult result = (await _auth.signInWithCredential(credential));
+
+    _user = result.user;
+
+    setState(() {
+      isSignIn = true;
+    });
+  }
+
+  Future<void> googleSignOut() async {
+    await _auth.signOut().then((onValue) {
+      _googleSignIn.signOut();
+      setState(() {
+        isSignIn = false;
+      });
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return MaterialApp(
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Scaffold(
+          appBar: AppBar(
+            title: Text("Google Authentication"),
+          ),
+          body: isSignIn
+              ? Center(
+                  child: Column(
+                    children: [
+                      CircleAvatar(
+                        backgroundImage: NetworkImage(_user.photoUrl),
+                      ),
+                      Text(_user.displayName),
+                      OutlinedButton(
+                        onPressed: () {},
+                        child: Text("logout"),
+                      ),
+                    ],
+                  ),
+                )
+              : Center(
+                  child: OutlinedButton(
+                    onPressed: () {},
+                    child: Text("Sign in with Google"),
+                  ),
+                )),
+    );
   }
 }
-
